44f71fbbb41f4a1e0018a8e0ec051ab33699c5b1
makdenis <makdenis.1997@gmail.com>
2018-08-31 01:24:32 +0300

    fixes

--- source/sort.js
+++ source/sort.js
@@ -1,20 +1,15 @@
 const sort = (words) => {
             let wordArray = words.split(" ");
-
-            for (let i = 0; i < wordArray.length; i++) {
-                let str = wordArray[i].toLowerCase();
-                let sortedWord = str.split("").sort(function (s1, s2) {
+            let newWordArray = wordArray.map((elem) => {
+                let str = elem.toLowerCase();
+                let sortedWord = str.split("").sort((s1, s2) => {
                     return s1.localeCompare(s2)
-
                 }).join("");
-
-                wordArray[i] = sortedWord[0].toUpperCase() + sortedWord.slice(1);
-            }
-
-            wordArray.sort(function (s1, s2) {
+                elem = [sortedWord[0].toUpperCase(), ...sortedWord.slice(1)].join("");
+                return elem
+            });
+            newWordArray.sort((s1, s2) => {
                 return s1.localeCompare(s2)
             });
-
-            wordArray.join(" ");
-            return wordArray.join(" ");
+            return newWordArray.join(" ");
         };
