2efaa464f4b9060360f68b380cf25a98e67817cc
makdenis <makdenis.1997@gmail.com>
2018-09-02 14:07:03 +0300

    small changes

--- source/sort.js
+++ source/sort.js
@@ -1,11 +1,8 @@
 const sort = (words) => {
     return words.split(" ").map((elem) => {
-        const str = elem.toLowerCase();
-        const sortedWord = str.split("").sort((s1, s2) => s1.localeCompare(s2)
+        const sortedWord = elem.toLowerCase().split("").sort((s1, s2) => s1.localeCompare(s2)
         ).join("");
-
         return [sortedWord[0].toUpperCase(), ...sortedWord.slice(1)].join("");
 
     }).sort((s1, s2) => s1.localeCompare(s2)).join(" ");
-
 };
