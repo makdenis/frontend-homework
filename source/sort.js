0368d0061ec94300b393cfcb44ff95cf8a60db17
makdenis <makdenis.1997@gmail.com>
2018-09-02 13:59:06 +0300

    fixes #2

--- source/sort.js
+++ source/sort.js
@@ -1,15 +1,11 @@
 const sort = (words) => {
-            let wordArray = words.split(" ");
-            let newWordArray = wordArray.map((elem) => {
-                let str = elem.toLowerCase();
-                let sortedWord = str.split("").sort((s1, s2) => {
-                    return s1.localeCompare(s2)
-                }).join("");
-                elem = [sortedWord[0].toUpperCase(), ...sortedWord.slice(1)].join("");
-                return elem
-            });
-            newWordArray.sort((s1, s2) => {
-                return s1.localeCompare(s2)
-            });
-            return newWordArray.join(" ");
-        };
+    return words.split(" ").map((elem) => {
+        const str = elem.toLowerCase();
+        const sortedWord = str.split("").sort((s1, s2) => s1.localeCompare(s2)
+        ).join("");
+
+        return [sortedWord[0].toUpperCase(), ...sortedWord.slice(1)].join("");
+
+    }).sort((s1, s2) => s1.localeCompare(s2)).join(" ");
+
+};
